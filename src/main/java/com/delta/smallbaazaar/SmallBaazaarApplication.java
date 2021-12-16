391b2874193a89deb1f9aebd9f7ffe2179a8b470
Kryptöñ <madhusudanbabar@gmail.com>
2021-12-16 11:04:47 +0530

    initial commit

---
+++ src/main/java/com/delta/smallbaazaar/SmallBaazaarApplication.java
@@ -0,0 +1,13 @@
+package com.delta.smallbaazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBaazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallBaazaarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delta/smallbaazaar/SmallBaazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delta.smallbaazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallBaazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
