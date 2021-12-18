4ded6ff385cecf76d82efd4d64cbaddc204bff9a
Hrucha-Wankhade <hrucha.wankhade@datametica.com>
2021-12-18 11:26:58 +0530

    orderController

---
+++ src/main/java/com/delta/smallbaazaar/controllers/orderController.java
@@ -0,0 +1,17 @@
+package com.delta.smallbaazaar.controllers;
+
+// By Hrucha
+import com.delta.smallbaazaar.entities.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class orderController {
+    @PostMapping("/order")
+
+    public String orderPlaced(@RequestParam Product product, Model model){
+        model.addAttribute("product",product);
+        return "order.html";
+    }
+}
