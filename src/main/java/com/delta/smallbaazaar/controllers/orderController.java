679982e3747856caa56626085718f29c90f2108e
Kryptöñ <madhusudanbabar@gmail.com>
2021-12-18 11:37:42 +0530

    project completed

--- src/main/java/com/delta/smallbaazaar/controllers/orderController.java
+++ src/main/java/com/delta/smallbaazaar/controllers/orderController.java
@@ -3,13 +3,15 @@ package com.delta.smallbaazaar.controllers;
 // By Hrucha
 import com.delta.smallbaazaar.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class orderController {
-    @PostMapping("/order")
 
+    @PostMapping("/order")
     public String orderPlaced(@RequestParam String product, Model model){
         model.addAttribute("product",product);
         return "order.html";
