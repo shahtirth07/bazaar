d9f52ded2939f24a4a7d7e57c40339cea17f5d6f
TIRTH SHAH <tirth.shah@DM1391.datametica.com>
2021-12-18 11:12:03 +0530

    t

--- src/main/java/com/delta/smallbaazaar/services/productService.java
+++ src/main/java/com/delta/smallbaazaar/services/productService.java
@@ -5,6 +5,7 @@ import com.delta.smallbaazaar.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+//tirth
 @Service
 public class productService {
 
